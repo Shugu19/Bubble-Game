8a5e7db0e297d911806519bf8a207db2cad52283
Shubham badhan <shubhambadhan.uk@gmail.com>
2024-04-27 15:07:42 +0100

    "Updated bubble game with score tracking, hit element and timer."

--- assests/script.js
+++ assests/script.js
@@ -1,15 +1,16 @@
 var timer = 60;
-var hit = 0;
 var score = 0;
+var hitrn = 0;
 
 function makeNewBubble() {
     var newbubble = "";
 
     for (var i = 1; i < 85; i++) {
-        newbubble += `<div class="bubble">${Math.floor(Math.random() * 100)}</div>`;
+        newbubble += `<div class="bubble">${Math.floor(Math.random() * 20)}</div>`;
     }
     document.querySelector("#bottompanel").innerHTML = newbubble;
 }
+
 function startTimer() {
     setInterval(function () {
         if (timer > 0) {
@@ -22,15 +23,26 @@ function startTimer() {
 
     }, 1000);
 }
-function makeNewHit(){
-    var hit = Math.floor(Math.random() * 100);
-    document.querySelector("#NewHit").innerHTML = hit;
+
+function makeNewHit() {
+    hitrn = Math.floor(Math.random() *20);
+    document.querySelector("#NewHit").textContent = hitrn;
 }
-function increaseScore(){
+
+function increaseScore() {
     score += 10;
     document.querySelector("#NewScore").innerHTML = score;
 }
 
+document.querySelector("#bottompanel").addEventListener("click", function (dets) {
+        var clickedNumber = Number(dets.target.textContent);
+        if (clickedNumber === hitrn) {
+            increaseScore();
+            makeNewHit();
+            makeNewBubble();
+        }
+    });
+
 makeNewBubble();
-startTimer();   
+startTimer();
 makeNewHit();
