5b4025d5a41492f3a251725445db0c9a39aee2a2
Shubham badhan <shubhambadhan.uk@gmail.com>
2024-04-30 12:33:17 +0100

    updated game with start button and fix some bugs

--- assests/script.js
+++ assests/script.js
@@ -2,6 +2,7 @@ var timer = 60;
 var score = 0;
 var hitrn = 0;
 
+
 function makeNewBubble() {
     var newbubble = "";
 
@@ -12,20 +13,20 @@ function makeNewBubble() {
 }
 
 function startTimer() {
-    setInterval(function () {
+    var timerInterval = setInterval(function () {
         if (timer > 0) {
             timer--;
             document.querySelector("#setTimer").innerHTML = timer;
+        } else {
+            clearInterval(timerInterval);
+            document.querySelector("#bottompanel").innerHTML = `<h1 id="endGame">Game Over</h1>`;
+            document.querySelector("#bottompanel").innerHTML += `<h2 id="NewScore">Your Score: ${score}</h2>`;
         }
-        else {
-            clearInterval(timer);
-        }
-
     }, 1000);
 }
 
 function makeNewHit() {
-    hitrn = Math.floor(Math.random() *20);
+    hitrn = Math.floor(Math.random() * 20);
     document.querySelector("#NewHit").textContent = hitrn;
 }
 
@@ -35,14 +36,17 @@ function increaseScore() {
 }
 
 document.querySelector("#bottompanel").addEventListener("click", function (dets) {
-        var clickedNumber = Number(dets.target.textContent);
-        if (clickedNumber === hitrn) {
-            increaseScore();
-            makeNewHit();
-            makeNewBubble();
-        }
-    });
+    var clickedNumber = Number(dets.target.textContent);
+    if (clickedNumber === hitrn) {
+        increaseScore();
+        makeNewHit();
+        makeNewBubble();
+    }
+});
 
-makeNewBubble();
-startTimer();
-makeNewHit();
+document.querySelector("#start").addEventListener("click", function () {
+    makeNewBubble();
+    startTimer();
+    makeNewHit();
+});
+
