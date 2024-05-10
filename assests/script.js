a24db2438e3dd83fc94ee7365f779e3cb2247ac2
Shubham badhan <shubhambadhan.uk@gmail.com>
2024-05-10 11:50:28 +0100

    Add  start again button.

--- assests/script.js
+++ assests/script.js
@@ -1,30 +1,27 @@
 var timer = 60;
 var score = 0;
 var hitrn = 0;
-
+var timerInterval;
 
 function makeNewBubble() {
     var newbubble = "";
 
-    for (var i = 1; i < 51; i++) {
-        newbubble += `<div class="bubble">${Math.floor(Math.random() * 15)}</div>`;
+    for (var i = 0; i < 78; i++) {
+        newbubble += `<div class="bubble">${Math.floor(Math.random() * 20)}</div>`;
     }
     document.querySelector("#bottompanel").innerHTML = newbubble;
 }
 
 function startTimer() {
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         if (timer > 0) {
             timer--;
             document.querySelector("#setTimer").innerHTML = timer;
         } else {
             clearInterval(timerInterval);
-            document.querySelector("#bottompanel").innerHTML = `<h1 id="endGame">Game Over</h1>`;
-            document.querySelector("#bottompanel").innerHTML += `<h2 id="finalScore">Your Score: ${score}</h2>`;
+            endGame();
         }
-        
     }, 1000);
-    
 }
 
 function makeNewHit() {
@@ -37,6 +34,30 @@ function increaseScore() {
     document.querySelector("#NewScore").innerHTML = score;
 }
 
+function endGame() {
+    document.querySelector("#bottompanel").innerHTML = `<h1 id="endGame">Game Over</h1>`;
+    document.querySelector("#bottompanel").innerHTML += `<h2 id="finalScore">Your Score: ${score}</h2>`;
+    if (!document.querySelector("#start")) { // Check if the button already exists
+        document.querySelector("#bottompanel").innerHTML += `<button id="startAgain">Start Again</button>`;
+        document.querySelector("#startAgain").addEventListener("click", function () {
+            resetGame();
+        });
+    }
+}
+
+
+
+
+function resetGame() {
+    clearInterval(timerInterval);
+    timer = 60;
+    score = 0;
+    document.querySelector("#setTimer").innerHTML = timer;
+    document.querySelector("#NewScore").innerHTML = score;
+    makeNewBubble();
+    makeNewHit();
+    startTimer(); // Restart timer
+}
 
 document.querySelector("#bottompanel").addEventListener("click", function (dets) {
     var clickedNumber = Number(dets.target.textContent);
@@ -49,6 +70,8 @@ document.querySelector("#bottompanel").addEventListener("click", function (dets)
 
 document.querySelector("#start").addEventListener("click", function () {
     makeNewBubble();
-    startTimer();
     makeNewHit();
+    startTimer();
 });
+
+
